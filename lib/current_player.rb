acb403fab6dc87a60fa717b412fe9c9f788ecc20
Jantastication O <georginaoregx@gmail.com>
2018-09-21 20:43:39 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 count = 0
 def turn_count(board)
   board.each do |index|
-    if index == "X" || index == "0"
+      if index == "X" || index == "0"
       count += 1
     end
   end
